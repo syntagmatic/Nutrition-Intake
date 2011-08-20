963c63206f2e07c79bf15b41f33ec1e78aaefb16
syntagmatic <kai.salmon.chang@gmail.com>
2011-08-20 15:49:08 -0700

    raw data processing

--- usda/nutrients.py
+++ usda/nutrients.py
@@ -8,13 +8,17 @@ sources = {
     'units': 'data/WEIGHT.txt',
     'footnotes': 'data/FOOTNOTE.txt',
     'langdesc': 'data/LANGDESC.txt',
+    'data': 'data/NUT_DATA.txt',
 }
+
+data = {}
 
 for k,v in sources.iteritems():
     source = csv.reader(open(v, 'rb'), delimiter='^', quotechar='~')
-    data = []
+    data[k] = []
     for i in source:
-        data.append(i)
-    f = open('json/' + k + '.js', 'w' )
-    f.write('var ' + k + " = " + json.dumps(data) + ";");
-    f.close()
+        data[k].append(i)
+
+# f = open('json/data.js', 'w' )
+# f.write('var data = ' + json.dumps(data['food_groups']) + ';')
+# f.close()
