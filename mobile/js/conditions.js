4e5e01580d90aa58b942bbafd03d3df2c9fea47c
syntagmatic <kai.salmon.chang@gmail.com>
2011-08-21 00:04:40 -0700

    sorted dropdown

--- mobile/js/conditions.js
+++ mobile/js/conditions.js
@@ -2,11 +2,19 @@ $(function() {
 
   var cond_template = _.template($('#conditions .template').html());
 
-  var fields_html = _(conditions).reduce(function(html, v, k) {
+  var cond_keys = _(conditions).values().sort();
+  var fields_html = _(cond_keys).reduce(function(html, cond) {
     return html += cond_template({
-      key: k,
-      value: v
+      key: getKey(cond, conditions),
+      value: cond
     });
   }, "");
   $('#conditions').append(fields_html);
+
+  function getKey(v, obj) {
+    for (k in obj) {
+      if (obj[k] == v)
+        return k;
+    }
+  };
 });
