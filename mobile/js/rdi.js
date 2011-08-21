777d01957846519919efc276ba5b967d594cfca2
mbecica <mary.becica@appdirect.com>
2011-08-21 14:57:53 -0700

    rdi styles and colors

--- mobile/js/rdi.js
+++ mobile/js/rdi.js
@@ -14,35 +14,28 @@ $(function() {
   food_keys = _(foods).keys().slice(800,803);
   var foods_html = "",
       nutrients_html = "";
-  /*
-  _(food_keys).each(function(k) {
-    foods_html += foods_template({
-      key: foods[k].name
-    });
-    _(foods[k]['nutrients']).each(function(nut) {
-      if (nut.id in recommendations) {
-        foods_html += nutrient_template({
-          key: nutrients[nut.id].name,
-          value: rdi(nut),
-          width: Math.round(rdi(nut))
-        });
-      }
-    });
+
+  window.colors = {};
+  
+  _(food_keys).each(function(k, i) {
+    colors['c'+k] = {
+      color:Raphael.getColor()
+    }
   });
-*/
   _(recommendations).each(function(v, k) {
     foods_html += foods_template({
       key: nutrients[k].name
     });
     foods_html += "<span class='row'>";
-    _(food_keys).each(function(f) {
+    _(food_keys).each(function(f, a) {
       _(foods[f]['nutrients']).each(function(nut) {
         if (nut.id == k) {
           var rdiW = rdi(nut);
           foods_html += nutrient_template({
             key: foods[f].name,
             value: rdiW,
-            width: rdiW
+            width: rdiW,
+            classN: 'c'+f
           });
         }
       });
@@ -50,7 +43,15 @@ $(function() {
     foods_html += "<span class='clear'></span></span><span class='clear'></span>";
   });
   $('#foods').append(foods_html);
-  
+   
+  $('bar').hover(function() {
+    var curClass = $(this).attr('class');
+    $('.'+curClass).css({'background-color':colors[curClass].color});
+    $('h2').text(foods[curClass.substring(1)].name);
+  }, function() {
+    $('bar').css({'background-color':'transparent'});
+    $('h2').text('');
+  });
   // percent of rdi for a nutrient
   function rdi(nutrient, value) {
     var fraction = nutrient.amount/recommendations[nutrient.id]['rdi'];
