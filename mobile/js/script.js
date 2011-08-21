78d8b188c1ca459729f9a72527bf914a7515d81a
Vaibhav Bhawsar <vaibhav@recombine.net>
2011-08-20 21:22:45 -0700

    fixed js-minor

--- mobile/js/script.js
+++ mobile/js/script.js
@@ -1,19 +1,22 @@
 $(function() {
-  var food_html, food_subset, food_template, nut_html, nut_template, template;
-  template = {};
-  nut_template = _.template($('#nutrients .template').html());
-  nut_html = _(nutrients).reduce(function(html, nut) {
+  var template = {};
+
+  var nut_template = _.template($('#nutrients .template').html());
+  var nut_html = _(nutrients).reduce(function(html, nut) {
     return html += nut_template({
       nutrient: nut.name
     });
   }, "");
-  food_subset = _(foods).keys().slice(0, 21);
-  food_template = _.template($('#foods .template').html());
-  food_html = _(food_subset).reduce(function(html, key) {
+  $('#nutrients').append(nut_html);
+
+  var food_subset = _(foods).keys().slice(0, 21);
+  var food_template = _.template($('#foods .template').html());
+  var food_html = _(food_subset).reduce(function(html, key) {
     return html += food_template({
       food: foods[key].name,
       nuts: foods[key].nutrients
     });
   }, "");
-  return $('#foods').append(food_html);
+  $('#foods').append(food_html);
+
 });
