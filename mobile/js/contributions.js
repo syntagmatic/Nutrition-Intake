9d3a989cd15dd1b9502fdb24bf0439cfcf1ef982
Vaibhav Bhawsar <vaibhav@recombine.net>
2011-08-21 12:20:24 -0700

    wokring on contributions

---
+++ mobile/js/contributions.js
@@ -0,0 +1,11 @@
+var contributions = {
+
+	foodbycondition : function(conditionid) {
+		// recommendations.
+	},
+	update : function(){
+		//_.each(conditions,function(v,k){console.log(v,k)})
+	},
+	render : function(){
+	}
+}
--- mobile/js/rdi.js
+++ mobile/js/rdi.js
@@ -3,7 +3,7 @@ $(function() {
   var foods_template = _.template($('#foods .template').html());
   var nutrient_template = _.template($('#foods .inner-template').html());
 
-  food_keys = _(foods).keys().slice(800,821);
+  food_keys = _(foods).keys().slice(800,901);
   var foods_html = "";
   _(food_keys).each(function(k) {
     foods_html += foods_template({
