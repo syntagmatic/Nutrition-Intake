f67c90b973c0ef5c9131c3068a82c8291b4f0357
mbecica <mary.becica@appdirect.com>
2011-08-21 00:34:47 -0700

    building food to health object for bar charts

--- mobile/js/script.js
+++ mobile/js/script.js
@@ -1,4 +1,19 @@
 $(function() {
+  window.foodHealth = {};
+  _.each(foods, function(val, key, id) {
+    foodHealth[key] = {};
+    _.each(val.nutrients, function(val2, key2) {
+      if (recommendations[val2.id]) {
+        var curHealth = 0,
+            curNut = recommendations[val2.id];
+        _.each(curNut.dos, function(val3, key3) {
+          foodHealth[key][conditions[val3]] || (foodHealth[key][conditions[val3]] = 0);
+          foodHealth[key][conditions[val3]] += 1;
+        });
+      }
+    });
+  });
+
   var template = {};
 
   var nut_template = _.template($('#nutrients .template').html());
@@ -8,14 +23,12 @@ $(function() {
       nutrient: nut.name
     });
   }, "");
-  */
   var rec_html = _(recommendations).reduce(function(html, nut, k) {
     return html += nut_template({
       nutrient: nutrients[k].name
     });
   });
-  //$('#nutrients').append(rec_html);
-
+  $('#nutrients').append(rec_html);
   var food_subset = _(foods).keys().slice(0, 21);
   var food_template = _.template($('#foods .template').html());
   var food_html = _(food_subset).reduce(function(html, key) {
@@ -26,4 +39,5 @@ $(function() {
   }, "");
   $('#foods').append(food_html);
 
+  */
 });
