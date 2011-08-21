67da167cad5c963fd29aae41ce62c05840e01b40
syntagmatic <kai.salmon.chang@gmail.com>
2011-08-21 14:25:24 -0700

    dropdown in rdi

--- mobile/js/rdi.js
+++ mobile/js/rdi.js
@@ -2,6 +2,50 @@ $(function() {
 
   var foods_template = _.template($('#foods .template').html());
   var nutrient_template = _.template($('#foods .inner-template').html());
+  var foodpick_template = _.template($('#foodpick .template').html());
+
+  var food_names = _(foods).map(function(v,k) {
+    return {
+      key: k,
+      name: v.name
+    };
+  });
+
+  var food_keys = _(food_names).chain()
+                               .sortBy(function(i) { return i.name })
+                               .pluck('key')
+                               .value();
+
+  // Dropdown
+  var foodpick_html = "";
+  _(food_keys).each(function(k) {
+    foodpick_html += foodpick_template({
+      key: k,
+      value: foods[k].name
+    });
+  });
+  $('#foodpick').html(foodpick_html);
+
+  $('#foodpick').change(function() {
+    var picked_food = [$(this).val()];
+    //render(picked_food);
+  });
+
+
+  // Dropdown
+  var foodpick_html = "";
+  _(food_keys).each(function(k) {
+    foodpick_html += foodpick_template({
+      key: k,
+      value: foods[k].name
+    });
+  });
+  $('#foodpick').html(foodpick_html);
+
+  $('#foodpick').change(function() {
+    var picked_food = [$(this).val()];
+    render(picked_food);
+  });
 
   // Update RDI values if form submission
   var rdi_updates = getUrlVars();
